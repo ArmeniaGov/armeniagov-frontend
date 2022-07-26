758d137911d27ba53334f202b16bffd26dd8208c
Mark Amiraghyan <amiraghyan03@gmail.com>
2022-07-26 19:16:33 +0400

    (feat): Generate armeniagov-frontend-website project

---
+++ apps/armeniagov-frontend-website/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'armeniagov-frontend-website',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/armeniagov-frontend-website',
+};
