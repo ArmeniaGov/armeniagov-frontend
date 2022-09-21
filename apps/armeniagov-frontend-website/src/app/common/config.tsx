cb0a36b6d2f38280b49dfadc18e401d48cb04067
Vahagn Zaqaryan <vahagn.zakaryan@tumo.org>
2022-09-22 00:01:11 +0400

    feat: add common config for nav menu

---
+++ apps/armeniagov-frontend-website/src/app/common/config.tsx
@@ -0,0 +1,188 @@
+import {
+    AccordionDoc,
+    BackLinkDoc,
+    BreadcrumbsDoc,
+    ButtonDoc,
+    CharacterCountDoc,
+    CheckboxesDoc,
+    CookieBannerDoc,
+    DateInputDoc,
+    DetailsDoc,
+    ErrorMessageDoc,
+    ErrorSummaryDoc,
+    FieldsetDoc,
+    FileUploadDoc,
+    FooterDoc,
+    HeaderDoc,
+    InsetTextDoc,
+    NotificationBannerDoc,
+    PaginationDoc,
+    PanelDoc,
+    PhaseBannerDoc,
+    RadiosDoc,
+    SelectDoc,
+    SkipLinkDoc,
+    SummaryListDoc,
+    TableDoc,
+    TabsDoc,
+    TagDoc,
+    TextInputDoc,
+    TextareaDoc,
+    WarningTextDoc
+} from "../modules/docs";
+
+export default {
+    subNaviagation: [
+        {
+            "title": "Accordion",
+            "element": <AccordionDoc/>,
+            "path": "/components/accordion"
+        },
+        {
+            "title": "Back link",
+            "element": <BackLinkDoc/>,
+            "path": "/components/back-link"
+        },
+        {
+            "title": "Breadcrumbs",
+            "element": <BreadcrumbsDoc/>,
+            "path": "/components/breadcrumbs"
+        },
+        {
+            "title": "Button",
+            "element": <ButtonDoc/>,
+            "path": "/components/button"
+        },
+        {
+            "title": "Character count",
+            "element": <CharacterCountDoc/>,
+            "path": "/components/character-count"
+        },
+        {
+            "title": "Checkboxes",
+            "element": <CheckboxesDoc/>,
+            "path": "/components/checkboxes"
+        },
+        {
+            "title": "Cookie banner",
+            "element": <CookieBannerDoc/>,
+            "path": "/components/cookie-banner"
+        },
+        {
+            "title": "Date input",
+            "element": <DateInputDoc/>,
+            "path": "/components/date-input"
+        },
+        {
+            "title": "Details",
+            "element": <DetailsDoc/>,
+            "path": "/components/details"
+        },
+        {
+            "title": "Error message",
+            "element": <ErrorMessageDoc/>,
+            "path": "/components/error-message"
+        },
+        {
+            "title": "Error summary",
+            "element": <ErrorSummaryDoc/>,
+            "path": "/components/error-summary"
+        },
+        {
+            "title": "Fieldset",
+            "element": <FieldsetDoc/>,
+            "path": "/components/fieldset"
+        },
+        {
+            "title": "File upload",
+            "element": <FileUploadDoc/>,
+            "path": "/components/file-upload"
+        },
+        {
+            "title": "Footer",
+            "element": <FooterDoc/>,
+            "path": "/components/footer"
+        },
+        {
+            "title": "Header",
+            "element": <HeaderDoc/>,
+            "path": "/components/header"
+        },
+        {
+            "title": "Inset text",
+            "element": <InsetTextDoc/>,
+            "path": "/components/inset-text"
+        },
+        {
+            "title": "Notification banner",
+            "element": <NotificationBannerDoc/>,
+            "path": "/components/notification-banner"
+        },
+        {
+            "title": "Pagination",
+            "element": <PaginationDoc/>,
+            "path": "/components/pagination"
+        },
+        {
+            "title": "Panel",
+            "element": <PanelDoc/>,
+            "path": "/components/panel"
+        },
+        {
+            "title": "Phase banner",
+            "element": <PhaseBannerDoc/>,
+            "path": "/components/phase-banner"
+        },
+        {
+            "title": "Radios",
+            "element": <RadiosDoc/>,
+            "path": "/components/radios"
+        },
+        {
+            "title": "Select",
+            "element": <SelectDoc/>,
+            "path": "/components/select"
+        },
+        {
+            "title": "Skip link",
+            "element": <SkipLinkDoc/>,
+            "path": "/components/skip-link"
+        },
+        {
+            "title": "Summary list",
+            "element": <SummaryListDoc/>,
+            "path": "/components/summary-list"
+        },
+        {
+            "title": "Table",
+            "element": <TableDoc/>,
+            "path": "/components/table"
+        },
+        {
+            "title": "Tabs",
+            "element": <TabsDoc/>,
+            "path": "/components/tabs"
+        },
+        {
+            "title": "Tag",
+            "element": <TagDoc/>,
+            "path": "/components/tag"
+        },
+        {
+            "title": "Text input",
+            "element": <TextInputDoc/>,
+            "path": "/components/text-input"
+        },
+        {
+            "title": "Textarea",
+            "element": <TextareaDoc/>,
+            "path": "/components/textarea"
+        },
+        {
+            "title": "Warning text",
+            "element": <WarningTextDoc/>,
+            "path": "/components/warning-text"
+        }
+    ]
+}
+
