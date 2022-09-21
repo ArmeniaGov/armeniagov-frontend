8f45100bffe7351874db1ca61b049d0335371928
Vahagn Zaqaryan <vahagn.zakaryan@tumo.org>
2022-09-21 23:59:25 +0400

    feat: add routes in main

--- apps/armeniagov-frontend-website/src/main.tsx
+++ apps/armeniagov-frontend-website/src/main.tsx
@@ -1,16 +1,48 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 
-import App from './app/app';
+import {
+  createBrowserRouter,
+  redirect,
+  RouterProvider,
+} from "react-router-dom";
+
+import config from './app/common/config';
+
+import ComponentsIndex from './app/pages/components/index';
+import Components from './app/pages/components/components';
+
+import ErrorPage from './app/error-page';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    loader: () => redirect('/components'),
+    errorElement: <ErrorPage/>
+  },
+  {
+    path: '/components',
+    element: <Components/>,
+    errorElement: <ErrorPage/>,
+    children: [
+      {
+        index: true,
+        element: <ComponentsIndex/>
+      },
+      ...config.subNaviagation.map(({ path, element }) => ({
+        path,
+        element
+      }))
+    ]
+  }
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </StrictMode>
 );
