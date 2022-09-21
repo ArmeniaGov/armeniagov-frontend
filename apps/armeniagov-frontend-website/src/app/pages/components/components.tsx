fd2bf5c3ece420b27c8a1afd5ba970999849ba20
Vahagn Zaqaryan <vahagn.zakaryan@tumo.org>
2022-09-22 00:00:54 +0400

    feat: add components page route

---
+++ apps/armeniagov-frontend-website/src/app/pages/components/components.tsx
@@ -0,0 +1,29 @@
+import { Outlet } from "react-router-dom";
+import { Header, Footer } from '@armeniagov/components-react';
+
+import config from '../../common/config';
+import { App, AppNav, AppContent } from '../../common/components';
+import { SubNavigation } from '../../common/components';
+
+
+export function Components() {
+  return (
+    <>
+      <Header serviceName='ArmeniaGov Components'/>
+
+      <App>
+        <AppNav>
+          <SubNavigation navigation={config.subNaviagation}></SubNavigation>
+        </AppNav>
+
+        <AppContent>
+          <Outlet></Outlet>
+        </AppContent>
+      </App>
+
+      <Footer/>
+    </>
+  );
+}
+
+export default Components;
---
+++ apps/armeniagov-frontend-website/src/app/pages/components/index.tsx
@@ -0,0 +1,25 @@
+export function ComponentsIndex() {
+  return (
+    <>
+      <h1>Components</h1>
+
+      <p>
+        Components are reusable parts of the user interface that have been made to support a variety of applications.
+      </p>
+
+      <p>
+        You can use individual components in many different patterns and contexts. For example, you can use the text input component to ask for an email address, a National Insurance number or someone’s name.
+      </p>
+
+      <p>
+        Each component in the GOV.UK Design System has coded examples. If you’re using the GOV.UK Prototype Kit, or have included GOV.UK Frontend in your build, coded examples will render the same as in the Design System.
+      </p>
+
+      <p>
+        The GOV.UK Design System’s code is public and freely available under a Massachusetts Institute of Technology (MIT) license. You can find our code repositories on GitHub, where we code in the open.
+      </p>
+    </>
+  );
+}
+
+export default ComponentsIndex;
