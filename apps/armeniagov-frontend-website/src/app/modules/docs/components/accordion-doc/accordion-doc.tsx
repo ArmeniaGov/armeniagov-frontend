62e948f51d6dc1b1700aa79aeb600788239f8e12
Mark Amiraghyan <amiraghyan03@gmail.com>
2022-09-22 19:36:22 +0400

    feat: add accordion doc

--- apps/armeniagov-frontend-website/src/app/modules/docs/components/accordion-doc/accordion-doc.tsx
+++ apps/armeniagov-frontend-website/src/app/modules/docs/components/accordion-doc/accordion-doc.tsx
@@ -1,18 +1,65 @@
 import { Accordion } from "@armeniagov/components-react";
+import { CopyBlock, dracula, atomOneDark } from "react-code-blocks";
 
 /* eslint-disable-next-line */
 // export interface AccordionDocProps {}
 
+const code = 
+`<Accordion sections={
+    [
+        {
+            header: 'Writing well for the web',
+            content: <p className="armeniagov-body">This is the content for Writing well for the web.</p>
+        },
+        {
+            header: 'Assembling the tech team',
+            summary: 'After understandning the requirements',
+            content: <p className="armeniagov-body">This is the content for Assembling the tech team</p>
+        },
+        {
+            header: 'Understanding the requirements',
+            summary: 'Valuing the users\\' opinion',
+            content: <p className="armeniagov-body">This is the content for Understanding the requirements</p>
+        }
+    ]
+}/>`;
+
+console.log(CopyBlock);
+
 export function AccordionDoc() {
   return (
-    <Accordion sections={[{
-      header: 'Writing well for the web',
-      content: <p className="armeniagov-body">This is the content for Writing well for the web.</p>
-    }, {
-      header: 'This finally works lol',
-      summary: 'After countless hours',
-      content: <p className="armeniagov-body">Hello pals</p>
-    }]}/>
+    <>
+      <CopyBlock
+        text={code}
+        theme={atomOneDark}
+        startingLineNumber={1}
+        language="jsx"
+        showLineNumbers
+        codeBlock
+        wrapLines
+      />
+
+      <hr/>
+
+      <Accordion sections={
+        [
+          {
+            header: 'Writing well for the web',
+            content: <p className="armeniagov-body">This is the content for Writing well for the web.</p>
+          },
+          {
+            header: 'Assembling the tech team',
+            summary: 'After understandning the requirements',
+            content: <p className="armeniagov-body">This is the content for Assembling the tech team</p>
+          },
+          {
+            header: 'Understanding the requirements',
+            summary: 'Valuing the users\' opinion',
+            content: <p className="armeniagov-body">This is the content for Understanding the requirements</p>
+          }
+        ]
+      }/>
+    </>
   );
 }
 
