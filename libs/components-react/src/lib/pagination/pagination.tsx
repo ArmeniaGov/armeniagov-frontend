042256628abc03fc00d264b48e1ecebab2a903c4
Vahagn Zaqaryan <vahagn.zakaryan@tumo.org>
2022-09-23 00:55:26 +0400

    fix: update pagination texts

--- libs/components-react/src/lib/pagination/pagination.tsx
+++ libs/components-react/src/lib/pagination/pagination.tsx
@@ -41,7 +41,7 @@ export function Pagination(props: PaginationProps) {
               <svg className="armeniagov-pagination__icon armeniagov-pagination__icon--prev" xmlns="http://www.w3.org/2000/svg" height="13" width="15" aria-hidden="true" focusable="false" viewBox="0 0 15 13">
                 <path d="m6.5938-0.0078125-6.7266 6.7266 6.7441 6.4062 1.377-1.449-4.1856-3.9768h12.896v-2h-12.984l4.2931-4.293-1.414-1.414z"></path>
               </svg>
-              <span className="armeniagov-pagination__link-title">Previous</span>
+              <span className="armeniagov-pagination__link-title">Նախորդ</span>
             </a>
           </div>
         ) : ''
@@ -106,7 +106,7 @@ export function Pagination(props: PaginationProps) {
         (
           <div className="armeniagov-pagination__next" onClick={_ => changePage(currentPage + 1)}>
             <a className="armeniagov-link armeniagov-pagination__link" href="#" rel="next" onClick={e => e.preventDefault()}>
-              <span className="armeniagov-pagination__link-title">Next</span>
+              <span className="armeniagov-pagination__link-title">Հաջորդը</span>
               <svg className="armeniagov-pagination__icon armeniagov-pagination__icon--next" xmlns="http://www.w3.org/2000/svg" height="13" width="15" aria-hidden="true" focusable="false" viewBox="0 0 15 13">
                 <path d="m8.107-0.0078125-1.4136 1.414 4.2926 4.293h-12.986v2h12.896l-4.1855 3.9766 1.377 1.4492 6.7441-6.4062-6.7246-6.7266z"></path>
               </svg>
